f35711a646c1e4666f0ee3a83865003be10feaa5
Jon Kevin Goncalves Hurtado <jonkevin.goncalves@servosa.com>
2025-04-25 16:28:46 +0200

    Added files

---
+++ Assets/Radial Progress Bar UI Pack/Scripts/RadialIndicatorClick.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class RadialIndicatorClick : MonoBehaviour
+{
+    [Header("Radial Timers")]
+    [SerializeField] private float indicatorTimer = 1.0f;
+    [SerializeField] private float maxIndicatorTimer = 10.0f;
+
+    [Header("UI Indicator")]
+    [SerializeField] private Image radialIndicator = null;
+
+    [Header("Key Codes")]
+    [SerializeField] private KeyCode selectKey = KeyCode.Mouse0;
+
+    [Header("Unity Event")]
+    [SerializeField] private UnityEvent myEvent = null;
+
+    private bool shouldUpdate = false;
+
+    void Update()
+    {
+        if (Input.GetKey(selectKey))
+        {
+            shouldUpdate = false;
+            indicatorTimer -= Time.deltaTime;
+            radialIndicator.enabled = true;
+            radialIndicator.fillAmount = indicatorTimer;
+
+            if (indicatorTimer <= 0)
+            {
+                indicatorTimer = maxIndicatorTimer;
+                radialIndicator.fillAmount = maxIndicatorTimer;
+                radialIndicator.enabled = false;
+                myEvent.Invoke();
+            }
+        }
+
+        else
+        {
+            if (shouldUpdate)
+            {
+                indicatorTimer += Time.deltaTime;
+                radialIndicator.fillAmount = indicatorTimer;
+
+                if (indicatorTimer >= maxIndicatorTimer)
+                {
+                    indicatorTimer = maxIndicatorTimer;
+                    radialIndicator.fillAmount = maxIndicatorTimer;
+                    radialIndicator.enabled = false;
+                    shouldUpdate = false;
+                }
+            }
+        }
+
+        if (Input.GetKeyUp(selectKey))
+        {
+            shouldUpdate = true;
+        }
+    }
+}
