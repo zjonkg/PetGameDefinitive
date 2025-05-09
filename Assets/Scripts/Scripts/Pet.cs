2bc9eac0020c2ef0b7bfaa8a5d63e8c57b883083
Liminessie <114174026+Liminessie@users.noreply.github.com>
2025-05-09 19:24:48 +0200

    Canvas modificado, Panel superior

---
+++ Assets/Scripts/Scripts/Pet.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    private string name;
+
+    private int hunger;
+
+    private int grooming;
+
+    private int nuts;
+
+    private int happiness;
+
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
