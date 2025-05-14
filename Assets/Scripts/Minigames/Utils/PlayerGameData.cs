3e7491d466f0003ac459072fcc1bb93adfc20f68
Jon Kevin Goncalves Hurtado <jonkevin.goncalves@servosa.com>
2025-05-14 15:51:57 +0200

    Added firebase

--- Assets/Scripts/Minigames/Utils/PlayerGameData.cs
+++ Assets/Scripts/Minigames/Utils/PlayerGameData.cs
@@ -1,4 +1,4 @@
-using UnityEditor.VersionControl;
+
 using UnityEngine;
 
 [System.Serializable]
--- Assets/Scripts/Utils/NotificationManager/NotificationManagers.cs
+++ Assets/Scripts/Utils/NotificationManager/NotificationManagers.cs
@@ -1,16 +1,47 @@
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Messaging;
 using UnityEngine;
 
 public class NotificationManagers : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private FirebaseApp app;
+
     void Start()
     {
-        
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Inicializar la instancia de Firebase
+                app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Ahora sí puedes suscribirte a los eventos
+                FirebaseMessaging.TokenReceived += OnTokenReceived;
+                FirebaseMessaging.MessageReceived += OnMessageReceived;
+
+                // Opcional: solicitar permiso para notificaciones
+                FirebaseMessaging.RequestPermissionAsync().ContinueWithOnMainThread(requestTask => {
+                    Debug.Log("Permiso para notificaciones solicitado.");
+                });
+
+                Debug.Log("Firebase inicializado correctamente.");
+            }
+            else
+            {
+                Debug.LogError($"No se pudieron resolver las dependencias de Firebase: {dependencyStatus}");
+            }
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTokenReceived(object sender, TokenReceivedEventArgs token)
     {
-        
+        Debug.Log("Token FCM recibido: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, MessageReceivedEventArgs e)
+    {
+        Debug.Log("Mensaje FCM recibido desde: " + e.Message.From);
+        Debug.Log("Contenido: " + e.Message.Notification?.Body ?? "Sin cuerpo");
     }
 }
